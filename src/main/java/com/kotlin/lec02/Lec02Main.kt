f524384d2703f77645d309f6ae4c86a7a6118269
kimseongmin <ksmles36@gmail.com>
2024-12-22 11:39:13 +0900

    코틀린에서의 null 다루기

--- src/main/java/com/kotlin/lec02/Lec02Main.kt
+++ src/main/java/com/kotlin/lec02/Lec02Main.kt
@@ -1,12 +1,44 @@
 package com.kotlin.lec02
 
-class Lec02Main {
-}
-
 fun main() {
 
+    val str: String = "APPLE"
+    println(startsWithA1(str))
+    println(startsWithA2(str))
+    println(startsWithA3(str))
 
+//    println(startsWithA1(null))
+    println(startsWithA2(null))
+    println(startsWithA3(null))
 
 }
 
+fun startsWithA1(str: String?): Boolean {
+//    if (str == null)
+//        throw IllegalArgumentException("String cannot be null")
+//
+//    return str.startsWith("A")
 
+
+    return str?.startsWith("A") ?: throw IllegalArgumentException()
+}
+
+fun startsWithA2(str: String?): Boolean? {
+//    if (str == null)
+//        return null
+//
+//    return str.startsWith("A")
+
+
+    return str?.startsWith("A")
+}
+
+fun startsWithA3(str: String?): Boolean {
+//    if (str == null)
+//        return false
+//
+//    return str.startsWith("A")
+
+
+    return str?.startsWith("A") ?: false
+}
